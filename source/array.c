6ee1c1f85b451c53dbfc5d80a5ba5df4353a0798
Noah Cohn <noahc42@gmail.com>
2021-07-20 21:20:29 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -4,17 +4,16 @@
 EM_JS(void,ma,(),{
 let r3g=Module.cwrap('r3g','number',['array']);
 let derp=[];
-let i,tx;
+var i,tx;
 let canvas=document.getElementById('rcanvas');
 let ctx = canvas.getContext('2d');
-let display=document.getElementById('disp').innerHTML;
 setInterval(function(){
 for(i=0;i<8;i++){
 setInterval(function(){
 derp=[i,1,2,3,66,5,6,7];
 tx=r3g(derp);
-display="";
-display=tx;
+document.getElementById('disp').innerHTML="";
+document.getElementById('disp').innerHTML=tx;
 ctx.fillStyle='rgb(255,255,33)';
 ctx.fillRect(100, 200, 222, 100);
 ctx.fillStyle='rgb('+tx+',255,33)';
