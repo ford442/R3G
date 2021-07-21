cef8f67dcb4f1810b5faaca6f9acf6b198f0656c
Noah Cohn <noahc42@gmail.com>
2021-07-21 00:47:30 -0700

    Update index.php

--- build/index.php
+++ build/index.php
@@ -10,14 +10,14 @@
 <iframe src="./bezz.htm" style="border-width:0px;position:absolute;top:0px;left:0px;right:0px;bottom:0px;overflow:hidden;z-index:999996;display:block;overflow-y:hidden;overflow-x:hidden;pointer-events: none;" id="circle"  title="Mask"></iframe><input type="checkbox" id="di" hidden/>
 <input type="button" id="btn" style="background-color: green;position: absolute;display: block;left: 5%;top: 50%;z-index: 999997;border:5px solid #e7e7e7;border-radius:50%;" onclick="document.getElementById('di').click();"></input>
 <div id="disp" style="position:absolute;font-size:xxx-large;color:orange;width:300px;height:300px;top:20%;left:50%;"></div>
-<canvas id="rcanvas" height="400" width="500" style="position:absolute;font-size:xxx-large;color:orange;width:700px;height:700px;">
+<canvas id="rcanvas" height="400" width="500" style="position:absolute;font-size:xxx-large;color:orange;width:700px;height:700px;top:20%;left:30%;">
 </body>
 <script>
 let bz=new BroadcastChannel('bez');
 document.getElementById('btn').addEventListener('click',function(){
 bz.postMessage({data: 222});});
 </script>
-<script async charset="utf-8" src="./r3g0.js"></script>
+<script async charset="utf-8" src="./r3g.js"></script>
 <script>
 let hi,wi;
 hi=window.innerHeight;
