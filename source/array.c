97ea0535397361bf36665aa7c63b787f96bfbdcc
Noah Cohn <noahc42@gmail.com>
2021-07-20 23:05:41 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -2,17 +2,18 @@
 #include<stdlib.h>
 #include<emscripten.h>
 EM_JS(void,ma,(),{
-var r3g=Module.cwrap('r3g','number',['number','array','number']);
-var derp=[];
-var i,tx;
-var canvas=document.getElementById('rcanvas');
-var ctx = canvas.getContext('2d');
+let r3g=Module.cwrap('r3g','number',['number','array','number']);
+let derp=[];
+let i,tx,tstr,tsti;
+let canvas=document.getElementById('rcanvas');
+let ctx = canvas.getContext('2d');
 setInterval(function(){
+ctx.fillStyle='rgb('+(Math.floor(Math.random(100)*255))+',255,33)';
 document.getElementById('disp').innerHTML="";
 for(i=1;i<8;i++){
 setInterval(function(){
-var tstr=i*10;
-var tsti=i*60;
+tstr=i*10;
+tsti=i*60;
 derp=[tsti,tstr,tsti,tstr,tsti,tstr,tsti,tstr];
 var llm=Math.random(111);
 llm=llm*10000;
@@ -21,18 +22,15 @@ llm=Math.floor(llm);
 llmm=Math.floor(llmm);
 llm=llm/10000;
 llmv=[llm,llmm];
-var tx=r3g(i,derp,llmv);
-document.getElementById('disp').innerHTML=tx;
-var canvas=document.getElementById('rcanvas');
-var ctx=canvas.getContext('2d');
-ctx.fillStyle='rgb(255,255,33)';
+tx=r3g(i,derp,llmv);
+ctx.clearRect(0, 0, 700, 700);
 ctx.fillRect(tx, 200, 222, 100);
-ctx.fillStyle='rgb('+tx+',255,33)';
-ctx.fillRect(100, (tx*100), tx, 100);
-},22);
+ctx.fillRect(100, tx, tx, 100);
+},50);
+document.getElementById('disp').innerHTML=tx;
 }},1000);
 });
-int r3g(int rr,register int ara[8],register float ch[2]){
+int r3g(int rr,register float ara[8],register float ch[2]){
 int ll=ara[rr]+(rr*10);
 ara[rr]=ll+ch[1];
 return ara[rr];
