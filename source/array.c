e20647ac2c2a301956c162b40644a1ae53c4e152
Noah Cohn <noahc42@gmail.com>
2021-07-20 20:46:51 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -7,18 +7,19 @@ let derp=[];
 let i,tx;
 let canvas=document.getElementById('rcanvas');
 let ctx = canvas.getContext('2d');
-ctx.fillStyle='rgb(0,69,255)';
 setInterval(function(){
 derp=[0,1,2,3,4,5,6,7];
 for(i=0;i<8;i++){
 derp[1]=(i*10)+100;
 derp[0]=(i*10)+100;
 tx=r3g(derp);
+ctx.fillStyle='rgb(0,tx,255)';
 ctx.fillRect(100, tx, 150, 100);
 }},1);
 });
 int r3g(register char ara[8]){
-return ara[1];
+int ll=ara[1];
+return ara[ll];
 }
 int main()
 {
