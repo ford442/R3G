a9a037f1008d790a9ea5f47b08a409e769605a51
Noah Cohn <noahc42@gmail.com>
2021-07-20 19:54:41 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -10,11 +10,10 @@ derp=[0,1,2,3,4,5,6,7];
 for (let i=0;i<8;i++){
 display=r3g(derp);
 derp[0]=derp[0]+1;
-derp[derp[0]]=derp[0]+1;
 }},1);
 });
 int r3g(register char ara[8]){
-return (ara[0],ara[1],ara[2],ara[3],ara[4],ara[5],ara[6],ara[7]);
+return ara[0];
 }
 int main()
 {
