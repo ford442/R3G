f75532a8b0ce1de1f567488a13e1629e6c7b75fb
Noah Cohn <noahc42@gmail.com>
2021-07-20 21:12:17 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -9,13 +9,15 @@ let canvas=document.getElementById('rcanvas');
 let ctx = canvas.getContext('2d');
 setInterval(function(){
 for(i=0;i<8;i++){
+setInterval(function(){
 derp=[i,1,2,3,66,5,6,7];
 tx=r3g(i,derp);
 ctx.fillStyle='rgb(255,255,33)';
 ctx.fillRect(100, 200, 222, 100);
 ctx.fillStyle='rgb('+tx+',255,33)';
 ctx.fillRect(100, 200, tx, 100);
-};},100);
+};},5);           
+},100);
 });
 int r3g(int ll, register char ara[8]){
 return ara[ll];
