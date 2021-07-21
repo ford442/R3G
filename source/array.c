b909026cc57a216b6352ab3c0f0b3cb48f63cb30
Noah Cohn <noahc42@gmail.com>
2021-07-21 01:33:20 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -10,12 +10,12 @@ let i,tx,tstr,tsti,c;
 let canvas=document.getElementById('rcanvas');
 let ctx = canvas.getContext('2d');
 setInterval(function(){
-for(c=1;c<60;c++){
-for(i=1;i<8;i++){
+for(c=0;c<60;c++){
+for(i=0;i<8;i++){
 setTimeout(function(){
 tstr=i*10;
 tsti=i*60;
-derp=[tsti,tstr,tsti,tstr,tsti,tstr,tsti,tstr];
+derp=[tsti,tstr,tsti,tstr,tsti,tstr,tsti,tstr,io,rgb1,rgb2];
 var llm=Math.random(111);
 llm=llm*10000;
 llmm=llm/2;
@@ -24,13 +24,14 @@ llmm=Math.floor(llmm);
 llm=llm/10000;
 llmv=[llm,llmm];
 tx=r3g(i,derp,llmv);
-var io=700-(tx/i);
-ctx.fillStyle="rgb("+(Math.floor(Math.random(255)))+",77,44)";
+io=700-(tx/i);
+rgb1=Math.floor(Math.random(255));
+rgb2=Math.floor(Math.random(255));
+ctx.fillStyle="rgb("+rgb1+",77,44)";
 ctx.fillRect(tx, io, 222, 100);
-ctx.fillStyle='rgb('+(Math.floor(Math.random(255)))+',77,44)';
+ctx.fillStyle='rgb(122,'+rgb2+',44)';
 ctx.fillRect(io, tx, tx, 100);
 document.getElementById('disp').innerHTML=tx;
-
 },16);
 }};
 ctx.clearRect(0, 0, 700, 700);
