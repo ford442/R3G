562f5f8862558f2d8d9897d828d37235367566ff
Noah Cohn <noahc42@gmail.com>
2021-07-20 22:36:29 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -12,8 +12,10 @@ document.getElementById('disp').innerHTML="";
 for(i=0;i<8;i++){
 setInterval(function(){
 derp=[66,1,2,3,66,5,6,7];
-tx=r3g(i,derp);
+var tx=r3g(i,derp);
 document.getElementById('disp').innerHTML=tx;
+var canvas=document.getElementById('rcanvas');
+var ctx = canvas.getContext('2d');
 ctx.fillStyle='rgb(255,255,33)';
 ctx.fillRect(tx, 200, 222, 100);
 ctx.fillStyle='rgb('+tx+',255,33)';
