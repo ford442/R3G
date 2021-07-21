272b64dc8727e9fd55e736d3a7d47115bdd033fa
Noah Cohn <noahc42@gmail.com>
2021-07-20 17:29:21 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -1,4 +1,3 @@
-
 #include "array.h"
 
 EM_JS(void,ma,(),
@@ -9,9 +8,8 @@ derp = [0,1,2,3,4,5,6,7];
 console.log(r3g(derp))
 });
 
-int r3g(ara) {
-register int words[8] = {ara[0],ara[1],ara[2],ara[3],ara[4],ara[5],ara[6],ara[7]};
-return words[7]);
+int r3g(register int ara[8]) {
+return ara[7]);
 }
 int main()
 {
