ca5e32d85cc16ed0c2c06c0884f69beb0f70326b
Noah Cohn <noahc42@gmail.com>
2021-07-20 19:17:34 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -4,12 +4,13 @@
 EM_JS(void,ma,(),{
 let display=document.getElementById('disp').innerHTML;
 let r3g=Module.cwrap('r3g','number',['number','array']);
+let i;
 setInterval(function(){
 let derp=[];
 derp = [0,1,2,3,4,5,6,7];
-for (blank<8;blank++;){
+for (i=0;i<8;i++;){
 display=r3g(derp);
-derp[0]=blank-1;
+derp[0]=derp[0]+1;
 }},1);
 });
 int r3g(register char ara[8]){
