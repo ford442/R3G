47773bd3cd561d9fe3da52a33d81e986dded2aeb
Noah Cohn <noahc42@gmail.com>
2021-07-20 18:36:10 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <emscripten.h>
-
+int blnk;
 EM_JS(void,ma,(),{
-let r3g=Module.cwrap('r3g','number',['array']);
+let display=document.getElementById('disp').innerHTML;
+let r3g=Module.cwrap('r3g','number',['number','array']);
+setInterval(function(){
 let derp=[];
+let blank=0;
 derp = [0,1,2,3,4,5,6,7];
-console.log(r3g(derp));
+for (blank<8;blank++;){
+display=r3g(blank,derp);
+}},1);
 });
-int r3g(register char ara[8]){
-printf(ara[7]);
-return ara[4];
+int r3g(blnk,register char ara[8]){
+return ara[blnk];
 }
 int main()
 {
