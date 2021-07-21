68ad5ddef0594fab692baabc57c834b2964f9a21
Noah Cohn <noahc42@gmail.com>
2021-07-20 17:30:18 -0700

    Update array.c

--- source/array.c
+++ source/array.c
@@ -1,15 +1,13 @@
 #include "array.h"
-
 EM_JS(void,ma,(),
 {
 let r3g = Module.cwrap('r3g', 'array', ['array']);
 let derp = [];
 derp = [0,1,2,3,4,5,6,7];
-console.log(r3g(derp))
+console.log(r3g(derp));
 });
-
 int r3g(register int ara[8]) {
-return ara[7]);
+return ara[7];
 }
 int main()
 {
